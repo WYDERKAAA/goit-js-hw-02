7128b7502b94fa6d84c6cc81e25df4868d56142c
WYDERKAAA <maciekwydra@yahoo.com>
2024-04-26 22:29:36 +0200

    done hw4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+function getShippingCost(country) {
+    
+    let price;
 
+    let message;
+
+    switch (country.toLowerCase()) {
+
+case `China`
+
+
+
+Chile — 250 
+Australia — 170 
+Jamaica — 120 
+
+
+    }
+
+
+
+
+}
