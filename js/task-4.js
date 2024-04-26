6aae97f723844de024fcbb8b105aeac4689759bd
WYDERKAAA <maciekwydra@yahoo.com>
2024-04-26 22:30:09 +0200

    1

--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,38 @@
 function getShippingCost(country) {
-    
-    let price;
+  let price;
 
-    let message;
+  let message;
 
-    switch (country.toLowerCase()) {
+  switch (country.toLowerCase()) {
+    case `china`:
+      price = 100;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
 
-case `China`
+    case `chile`:
+      price = 250;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
 
+    case `australia`:
+      price = 170;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
 
+    case `jamaica`:
+      price = 120;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
 
-Chile — 250 
-Australia — 170 
-Jamaica — 120 
+    default:
+      message = "Sorry, there is no delivery to your country";
+  }
 
-
-    }
-
-
-
-
+  return message;
 }
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
